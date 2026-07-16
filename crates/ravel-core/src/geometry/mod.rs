fafa01b6c4f494be3b5dad77c5423d9058001864
Naruse Nia <notshinsin0000@gmail.com>
2026-07-17 04:37:42 +0900

    feat: add geometry attribute operations

--- crates/ravel-core/src/geometry/mod.rs
+++ crates/ravel-core/src/geometry/mod.rs
@@ -7,10 +7,15 @@ mod attribute;
 mod container;
 mod field;
 pub mod names;
+pub mod ops;
 
 pub use attribute::{AttrName, AttributeArray, AttributeSet, AttributeType, GeometryError};
 pub use container::{Domain, Geometry, GeometrySummary, Primitive};
 pub use field::{
     AddField, BlendField, CurveRemapField, ExpressionField, FalloffField, FalloffShape, Field,
     FieldError, FieldValue, ImageSamplerField, MaxField, MultiplyField, NoiseField, apply_field,
+};
+pub use ops::{
+    AggregateMode, AttributeValue, GeometryOpError, PathSample, TransferMode, attribute_set,
+    attribute_transfer, path_sample, promote_attribute,
 };
